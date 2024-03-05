d27741d947a75b518c3947c57e2960133c85be73
Felipe Gallego <felipegallego600@gmail.com>
2024-03-04 20:09:12 -0500

    Resize

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var canvas = document.getElementById('main');
 var ctx = canvas.getContext('2d');
 
 var click = false;
-var first = false
+var first = false;
+var resize = false;
 canvas.addEventListener('mousedown', function (event) {
     click = true;
-    first = false
+    first = false;
+    resize = false;
 })
 canvas.addEventListener('mouseup', function () {
     click = false;
-    first = false
+    first = false;
+    resize = false;
 });
 
 canvas.addEventListener('dblclick', Seleccionar);
@@ -26,9 +29,10 @@ function update() {
     square.forEach(square => {
         ctx.lineWidth = 10;
         ctx.strokeStyle = square.border_color;
-        ctx.strokeRect(square.x, square.y, square.size, square.size);
+        ctx.strokeRect(square.x, square.y, square.sizeX, square.sizeY);
         ctx.fillStyle = square.background_color;
-        ctx.fillRect(square.x, square.y, square.size, square.size);
+        ctx.fillRect(square.x, square.y, square.sizeX, square.sizeY);
+
 
     });
 
@@ -39,8 +43,9 @@ function NewSquare() {
         id: Math.floor(Math.random() * 10000000000) + 1,
         x: 50,
         y: 50,
-        size: 100,
-        background_color: "#000000",
+        sizeX: 100,
+        sizeY: 100,
+        background_color: "#ffffff",
         border_color: "#000000",
     })
     update();
@@ -55,9 +60,9 @@ var ult;
 
 
 function Seleccionar(event) {
-    if(ult=1){
+    if (ult = 1) {
         cuadrado_select.border_color = "#000000"
-        square[index_select]=cuadrado_select
+        square[index_select] = cuadrado_select
     }
     ult = 1;
 
@@ -73,19 +78,19 @@ function Seleccionar(event) {
     // Mostrar las coordenadas en la consola
 
     cuadrado_select = square.find(data => {
-        return canvasX >= data.x && canvasX <= (data.x + data.size)
-            && canvasY >= data.y && canvasY <= (data.y + data.size);
+        return canvasX >= data.x && canvasX <= (data.x + data.sizeX)
+            && canvasY >= data.y && canvasY <= (data.y + data.sizeY);
     })
     if (cuadrado_select) {
         index_select = square.findIndex(data => data.id == cuadrado_select)
         cuadrado_select.border_color = "#ff5733"
-        square[index_select]=cuadrado_select
+        square[index_select] = cuadrado_select
         update()
     } else {
         index_select = undefined
     }
 
-    
+
 
 }
 
@@ -93,7 +98,7 @@ function Arrastrar(event) {
     if (click) {
         if (ult == 1) {
             cuadrado_select.border_color = "#000000"
-            square[index_select]=cuadrado_select
+            square[index_select] = cuadrado_select
         }
         ult = 0;
         // Obtener las coordenadas del clic
@@ -103,24 +108,32 @@ function Arrastrar(event) {
         var rect = canvas.getBoundingClientRect();
         var canvasX = x - rect.left;
         var canvasY = y - rect.top;
-        // Mostrar las coordenadas en la consola
         if (first) {
-            cuadrado_select.x = canvasX + difx
-            cuadrado_select.y = canvasY + dify
+            if (resize) {
+                cuadrado_select.sizeX = cuadrado_select.sizeX + -1 * (cuadrado_select.x - canvasX + cuadrado_select.sizeX);
+                cuadrado_select.sizeY = cuadrado_select.sizeY + -1 * (cuadrado_select.y - canvasY + cuadrado_select.sizeY);
+            } else if (!resize) {
+                cuadrado_select.x = canvasX + difx
+                cuadrado_select.y = canvasY + dify
+            }
             square[index_select] = cuadrado_select
             update()
+
         }
         if (!first) {
 
             cuadrado_select = square.find(data => {
-                return canvasX >= data.x && canvasX <= (data.x + data.size)
-                    && canvasY >= data.y && canvasY <= (data.y + data.size);
+                return canvasX >= data.x && canvasX <= (data.x + data.sizeX)
+                    && canvasY >= data.y && canvasY <= (data.y + data.sizeY);
             })
             if (cuadrado_select) {
                 first = true
                 index_select = square.findIndex(data => data.id == cuadrado_select)
                 difx = cuadrado_select.x - canvasX
                 dify = cuadrado_select.y - canvasY
+                if ((difx + cuadrado_select.sizeX) <= 20 && (dify + cuadrado_select.sizeY) <= 20) {
+                    resize = true
+                }
             } else {
                 index_select = undefined
             }
