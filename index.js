85da05a9c6caed602a6343460c034a8c2982123a
Felipe Gallego <felipegallego600@gmail.com>
2024-03-05 19:13:55 -0500

    Conexion

--- index.js
+++ index.js
@@ -35,10 +35,22 @@ var container = document.getElementById('container');
 // Definir las propiedades del cuadrado
 var square = [];
 var squareI = [];
+var vector= []
 
 // Función para dibujar el cuadrado
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+        console.log(vector)
+    vector.forEach(line=>{
+        ctx.moveTo(line.x_exit, line.y_exit);
+        ctx.lineTo(line.x_entry, line.y_entry);
+        //ctx.strokeStyle = '#ff0080';
+        ctx.strokeStyle = '#000000';
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    })
+    
     square.forEach(square => {
         ctx.lineWidth = 10;
         ctx.strokeStyle = square.border_color;
@@ -47,6 +59,8 @@ function update() {
         ctx.fillRect(square.x, square.y, square.sizeX, square.sizeY);
         ctx.drawImage(imagen_Info,square.x+square.sizeX-20, square.y,20, 20);
         ctx.drawImage(imagen_Resize,square.x+square.sizeX-20, square.y+square.sizeY-20,20, 20);
+
+
 
         ctx.fillStyle = square.text_color;
         ctx.font = '20px Arial';
@@ -95,11 +109,27 @@ function NewSquare() {
         background_color: "#ffffff",
         border_color: "#000000",
         text_color: "#000000",
-        text:"Default"
+        text:"Default",
+        conect:[],
 
     })
     squareI=square.slice().reverse();
     update();
+}
+
+
+function NewVector(id_entry,id_exit,x_entry,y_entry,x_exit,y_exit) {
+    let x=Math.floor(Math.random() * 10000000000) + 1
+    vector.push({
+        id:x,
+        id_entry:id_entry,
+        id_exit:id_exit,
+        x_entry:x_entry,
+        y_entry:y_entry,
+        x_exit:x_exit,
+        y_exit:y_exit,
+    })
+    return x
 }
 
 
@@ -135,8 +165,9 @@ function Seleccionar(event) {
         return canvasX >= data.x && canvasX <= (data.x + data.sizeX)
             && canvasY >= data.y && canvasY <= (data.y + data.sizeY);
     })
+    
     if (cuadrado_select) {
-        index_select = square.findIndex(data => data.id == cuadrado_select)
+        index_select = square.findIndex(data => data.id == cuadrado_select.id)
         square[index_select] = cuadrado_select
         difx = cuadrado_select.x - canvasX
         dify = cuadrado_select.y - canvasY
@@ -146,6 +177,7 @@ function Seleccionar(event) {
             container.classList.remove("oculto")
         }else{
             cuadrado_select.border_color = "#ff5733"
+            conectar(index_select)
         }
         update()
     } else {
@@ -176,6 +208,7 @@ function Arrastrar(event) {
             if(cuadrado_select){
                 cuadrado_select.border_color = "#000000"
                 square[index_select] = cuadrado_select
+                before_index=-1
             }
         }
         ult = 0;
@@ -190,11 +223,18 @@ function Arrastrar(event) {
             if (resize) {
                 cuadrado_select.sizeX = cuadrado_select.sizeX + -1 * (cuadrado_select.x - canvasX + cuadrado_select.sizeX);
                 cuadrado_select.sizeY = cuadrado_select.sizeY + -1 * (cuadrado_select.y - canvasY + cuadrado_select.sizeY);
+                if(cuadrado_select.sizeX<40){
+                    cuadrado_select.sizeX=40
+                }
+                if(cuadrado_select.sizeY<40){
+                    cuadrado_select.sizeY=40
+                }
             } else if (!resize) {
                 cuadrado_select.x = canvasX + difx
                 cuadrado_select.y = canvasY + dify
             }
             square[index_select] = cuadrado_select
+            reloadVector(cuadrado_select.id,cuadrado_select.x,cuadrado_select.y,cuadrado_select.sizeX,cuadrado_select.sizeY)
             update()
 
         }
@@ -220,6 +260,59 @@ function Arrastrar(event) {
     }
 }
 
+
+function reloadVector(id,x,y,sizeX,sizeY){
+    for (let i=0;i<vector.length;i++){
+        if(vector[i].id_entry==id){
+            vector[i].x_entry=x+sizeX /2
+            vector[i].y_entry=y+sizeY /2
+        }
+        if(vector[i].id_exit==id){
+            vector[i].x_exit=x+sizeX /2
+            vector[i].y_exit=y+sizeY /2
+        }
+    }
+
+}
+
+var before_cuadrado;
+var before_index=-1;
+
+function conectar(){
+    if(before_index==-1){
+        before_cuadrado=cuadrado_select;
+        before_index=index_select;
+    }
+    else{
+        if(before_index.id!=cuadrado_select.id){
+            let r=vector.findIndex(x=> x.id_entry==cuadrado_select.id&& x.id_exit==before_cuadrado.id)
+            if (r==-1){
+                
+                let id =NewVector(cuadrado_select.id,before_cuadrado.id,
+                    (cuadrado_select.x+cuadrado_select.sizeX/2),(cuadrado_select.y+cuadrado_select.sizeY/2),
+                    (before_cuadrado.x+before_cuadrado.sizeX/2),(before_cuadrado.y+before_cuadrado.sizeY/2))
+
+                    before_cuadrado.conect.push(id)
+                    cuadrado_select.conect.push(id)
+
+            }else{
+                let vectorid=vector[r]
+                let rbefore=before_cuadrado.conect.findIndex(x=>x==vectorid)
+                before_cuadrado.conect.splice(rbefore, 1);
+                let rselect=cuadrado_select.conect.findIndex(x=>x==vectorid)
+                cuadrado_select.conect.splice(rselect, 1);
+                vector.splice(r, 1);
+
+
+            }
+            square[before_index] = before_cuadrado
+            square[index_select] = cuadrado_select
+            before_index=-1
+            cuadrado_select.border_color = "#000000"
+        }
+
+    }
+}
 
 
 
